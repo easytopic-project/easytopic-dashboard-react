e95b6198c56ef6881f3edd656ce91a3958a14190
Bruno Juca <brunoolijuca@gmail.com>
2021-01-26 22:20:07 -0300

    Add router and theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Home } from "./pages"
+import Theme from "./theme/theme";
 
 function App() {
   return (
-    <div className="App">
+    <Theme>
+      <CssBaseline />
+      <div className="App">
+        <Router>
+          <Header>Easytopic</Header>
 
-    </div>
+          <Route exact path="/" component={Home} />
+
+        </Router>
+      </div>
+    </Theme>
   );
 }
 
