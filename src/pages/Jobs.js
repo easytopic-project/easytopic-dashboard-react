e03f7ef1a22938bb8d69739f69c05c4b9ce8d154
Bruno Juca <brunoolijuca@gmail.com>
2021-08-02 08:49:38 -0300

    Sorts jobs page

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,8 +1,20 @@
-import { Container, Table, TableBody, TableCell, TableContainer, TableRow, Typography, Paper, makeStyles, TableHead } from '@material-ui/core'
-import React, { useState, useEffect } from 'react'
-import { Link } from "react-router-dom"
-import API from '../api/API'
-import { useJobsContext } from '../contexts/JobsContext';
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+  Paper,
+  makeStyles,
+  TableHead,
+  TableSortLabel,
+} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import API from "../api/API";
+import { useJobsContext } from "../contexts/JobsContext";
 
 const useStyle = makeStyles((theme) => ({
   container: {
@@ -12,7 +24,7 @@ const useStyle = makeStyles((theme) => ({
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(3),
   },
-  headRow :{
+  headRow: {
     textDecoration: "none",
     backgroundColor: theme.palette.primary.main,
   },
@@ -23,6 +35,7 @@ const useStyle = makeStyles((theme) => ({
   row: {
     "&:hover": {
       backgroundColor: `${theme.palette.primary.dark} !important`,
+      cursor: "pointer",
     },
     textDecoration: "none",
     backgroundColor: theme.palette.primary.main,
@@ -45,45 +58,121 @@ const useStyle = makeStyles((theme) => ({
   },
   failed: {
     backgroundColor: "red",
-  }
+  },
 }));
 
-function Jobs() {
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-  const {jobsData} = useJobsContext();
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function Jobs() {
+  const { jobsData, setJobsData } = useJobsContext();
   const classes = useStyle();
+  const history = useHistory();
+
+  const [order, setOrder] = useState("desc");
+  const [orderBy, setOrderBy] = useState("status"); //id, type, version or status
+
+  const headCells = [
+    { id: "id", numeric: true, label: "ID" },
+    { id: "type", numeric: true, label: "Type" },
+    { id: "version", numeric: true, label: "Version" },
+    { id: "status", numeric: true, label: "Status" },
+    { id: "date", numeric: true, label: "Date" },
+  ];
+
+  function handleRequestSort(event, property) {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  }
+
+  function handleTableClick(id) {
+    history.push(`/jobs/${id}`);
+  }
 
   return (
     <Container maxWidth="md" className={classes.container}>
-      <Typography variant="h4" className={classes.title}>My Jobs</Typography>
+      <Typography variant="h4" className={classes.title}>
+        My Jobs
+      </Typography>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow className={classes.headRow}>
-              <TableCell className={classes.headCell} align="center">ID</TableCell>
-              <TableCell className={classes.headCell} align="center">Type</TableCell>
-              <TableCell className={classes.headCell} align="center">Version</TableCell>
-              <TableCell className={classes.headCell} align="center">Status</TableCell>
+              {headCells.map((headCell) => (
+                <TableCell
+                  key={headCell.id}
+                  className={classes.headCell}
+                  align="center"
+                  sortDirection={orderBy === headCell.id ? order : false}
+                >
+                  <TableSortLabel
+                    active={orderBy === headCell.id}
+                    direction={orderBy === headCell.id ? order : "asc"}
+                    onClick={(event) => handleRequestSort(event, headCell.id)}
+                  >
+                    {headCell.label}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {jobsData && jobsData.map((job) => 
-              <TableRow hover className={classes.row} key={job.id} component={Link} to={`/jobs/${job.id}`}>
-                <TableCell className={classes.cell} align="center">{job.id}</TableCell>
-                <TableCell className={classes.cell} align="center">{job.type.toUpperCase()}</TableCell>
-                <TableCell className={classes.cell} align="center">{job.version}</TableCell>
-                <TableCell className={classes.cell} align="center">
-                  <div className={`${classes.status} ${classes[job.status]}`}>
-                    {job.status.toUpperCase()}
-                  </div>   
-                </TableCell>
-              </TableRow>
-            )}
+            {jobsData &&
+              stableSort(jobsData, getComparator(order, orderBy)).map((job) => (
+                <TableRow
+                  hover
+                  className={classes.row}
+                  key={job.id}
+                  onClick={() => handleTableClick(job.id)}
+                >
+                  <TableCell className={classes.cell} align="center">
+                    {job.id}
+                  </TableCell>
+                  <TableCell className={classes.cell} align="center">
+                    {job.type.toUpperCase()}
+                  </TableCell>
+                  <TableCell className={classes.cell} align="center">
+                    {job.version}
+                  </TableCell>
+                  <TableCell className={classes.cell} align="center">
+                    <div className={`${classes.status} ${classes[job.status]}`}>
+                      {job.status.toUpperCase()}
+                    </div>
+                  </TableCell>
+                  <TableCell className={classes.cell} align="center">
+                    01/01/2021
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
     </Container>
-  )
+  );
 }
 
-export default Jobs
+export default Jobs;
