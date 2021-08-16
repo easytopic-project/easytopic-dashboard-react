1102a329b0474072ee45d70d22eae1e820a651ef
Bruno Juca <brunoolijuca@gmail.com>
2021-08-16 18:38:01 -0300

    Update enclosing providers and router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import { CssBaseline } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import AppRouter from "./AppRouter";
-import Header from "./components/Header";
-import GlobalContextProvider, {
-  useGlobalContext,
-} from "./contexts/GlobalContext";
+import GlobalContextProvider from "./contexts/GlobalContext";
 import JobsContextProvider from "./contexts/JobsContext";
-import { Home, Pipeline } from "./pages";
 import Theme from "./theme/theme";
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  Home,
+  PipelineForm,
+  Jobs,
+  PipelineSelection,
+  NotFound,
+  About,
+} from "./pages";
+import { useGlobalContext } from "./contexts/GlobalContext";
+import PipelineResult from "./pages/Pipeline/PipelineResult";
 
 function App() {
   return (
-    <SnackbarProvider
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "right",
-      }}
-    >
-      <GlobalContextProvider>
-        <JobsContextProvider>
-          <Theme>
-            <CssBaseline />
-            <div className="App">
-              <AppRouter />
-            </div>
-          </Theme>
-        </JobsContextProvider>
-      </GlobalContextProvider>
-    </SnackbarProvider>
+    <Router>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <GlobalContextProvider>
+          <JobsContextProvider>
+            <Theme>
+              <CssBaseline />
+              <div className="App">
+                <Header>Easytopic</Header>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route
+                    exact
+                    path="/pipelines"
+                    component={PipelineSelection}
+                  />
+                  <Route path="/pipelines/:id" component={PipelineForm} />
+                  <Route exact path="/jobs" component={Jobs} />
+                  <Route path="/jobs/:id" component={PipelineResult} />
+                  <Route path="/about" component={About} />
+                  <Route path="*" component={NotFound} />
+                </Switch>
+              </div>
+            </Theme>
+          </JobsContextProvider>
+        </GlobalContextProvider>
+      </SnackbarProvider>
+    </Router>
   );
 }
 
