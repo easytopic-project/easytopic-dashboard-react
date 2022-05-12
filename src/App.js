1b1699d55641c541c7f1f22ab82e0b7ec33cc4ea
Bruno Juca <brunoolijuca@gmail.com>
2022-05-12 13:54:52 -0300

    start refactor

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import LoginContextProvider from "./contexts/LoginContext";
 import NewPipeline from "./pages/NewPipeline";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import NewPipelineContextProvider from "./contexts/NewPipelineContext";
 
 function App() {
   return (
@@ -38,34 +39,36 @@ function App() {
             <LoginContextProvider>
               <JobsContextProvider>
                 <ModulesContextProvider>
-                  <Theme>
-                    <CssBaseline />
-                    <div className="App">
-                      <Header>M2P</Header>
-                      <Switch>
-                        <Route exact path="/" component={Home} />
-                        <PrivateRoute
-                          exact
-                          path="/pipelines"
-                          component={PipelineSelection}
-                        />
-                        <PrivateRoute
-                          path="/pipelines/:id"
-                          component={PipelineForm}
-                        />
-                        <PrivateRoute exact path="/jobs" component={Jobs} />
-                        <PrivateRoute
-                          path="/jobs/:id"
-                          component={PipelineResult}
-                        />
-                        <Route path="/about" component={About} />
-                        <PrivateRoute path="/modules" component={Modules} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/newpipeline" component={NewPipeline} />
-                        <Route path="*" component={NotFound} />
-                      </Switch>
-                    </div>
-                  </Theme>
+                  <NewPipelineContextProvider>
+                    <Theme>
+                      <CssBaseline />
+                      <div className="App">
+                        <Header>M2P</Header>
+                        <Switch>
+                          <Route exact path="/" component={Home} />
+                          <PrivateRoute
+                            exact
+                            path="/pipelines"
+                            component={PipelineSelection}
+                          />
+                          <PrivateRoute
+                            path="/pipelines/:id"
+                            component={PipelineForm}
+                          />
+                          <PrivateRoute exact path="/jobs" component={Jobs} />
+                          <PrivateRoute
+                            path="/jobs/:id"
+                            component={PipelineResult}
+                          />
+                          <Route path="/about" component={About} />
+                          <PrivateRoute path="/modules" component={Modules} />
+                          <Route path="/login" component={Login} />
+                          <Route path="/newpipeline" component={NewPipeline} />
+                          <Route path="*" component={NotFound} />
+                        </Switch>
+                      </div>
+                    </Theme>
+                  </NewPipelineContextProvider>
                 </ModulesContextProvider>
               </JobsContextProvider>
             </LoginContextProvider>
